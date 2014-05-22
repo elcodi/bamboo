5fb2f2d89f1691dae05b1f7b2558467b56e7fe01
Aldo Chiecchia <aldo.chiecchia@befactory.es>
2014-05-22 13:03:54 +0200

    Added static content controller

---
+++ src/Store/StoreCoreBundle/Controller/StaticController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author ##author_placeholder
+ * @version ##version_placeholder##
+ */
+
+namespace Store\StoreCoreBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StaticController extends Controller
+{
+    /**
+     * Static content controller
+     *
+     * @return array
+     *
+     * @Route(
+     *      path = "/content",
+     *      name = "store_static_content"
+     * )
+     * @Template()
+     */
+    public function contentAction()
+    {
+        return [];
+    }
+}
