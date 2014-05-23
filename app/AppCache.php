d1590bb68f80b5ce6fae000cb3775096513c5e16
Aldo Chiecchia <aldo.chiecchia@befactory.es>
2014-05-23 10:04:55 +0200

    Added license information in headers

--- app/AppCache.php
+++ app/AppCache.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * This distribution is just a basic e-commerce implementation based on
+ * Elcodi project.
+ *
+ * Feel free to edit it, and make your own
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author ##author_placeholder
+ * @version ##version_placeholder##
+ */
+
 require_once __DIR__.'/AppKernel.php';
 
 use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,7 +1,20 @@
 <?php
 
 /**
- * @header_placeholder
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * This distribution is just a basic e-commerce implementation based on
+ * Elcodi project.
+ *
+ * Feel free to edit it, and make your own
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author ##author_placeholder
+ * @version ##version_placeholder##
  */
 
 use Symfony\Component\HttpKernel\Kernel;
