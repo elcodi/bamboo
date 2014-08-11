668a6f0512bb9112ea8ae204eb61e301d9375526
mmoreram <marc.morera@befactory.es>
2014-08-11 19:46:37 +0200

    Removed unused Use Statement

--- web/app.php
+++ web/app.php
@@ -3,8 +3,6 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-use Elcodi\Admin\App;
-
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance.
