55e702ca2337a482c86ecd90578add75d0a7d606
Toni Pinel <tonipinel@gmail.com>
2015-01-09 11:46:56 +0100

    Move all templates to the new StoreTemplateBundle. The old templates are including the new ones to ensure everything is working but it should be removed.

---
+++ src/Elcodi/StoreTemplateBundle/StoreTemplateBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tonipinel
+ * Date: 7/1/15
+ * Time: 17:34
+ */
+
+namespace Elcodi\StoreTemplateBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StoreTemplateBundle extends Bundle
+{
+
+}
