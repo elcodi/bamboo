7a4e847d4401f69486478634d5edc99361a18834
Toni Pinel <tonipinel@gmail.com>
2015-01-13 11:43:02 +0100

    Add variants in modal window and show added images in modal window

---
+++ src/Elcodi/AdminCoreBundle/Resources/public/js/modules/variants.js
@@ -0,0 +1,55 @@
+TinyCore.AMD.define('variants', ['devicePackage','modal' ], function () {
+    return {
+        modal:  TinyCore.Module.instantiate( 'modal' ),
+        mediator : TinyCore.Toolbox.request( 'mediator' ),
+        onStart: function () {
+            this.mediator.subscribe( ['response:success'], this.updateVariants, this );
+
+        },
+        bindLinks: function() {
+
+            var self = this;
+
+            $('a' , '#variants-list').on('click',function(event) {
+
+                if ( this.className.indexOf('icon-trash-o') == -1 ) {
+
+                    event.preventDefault();
+
+                    self.modal.open({
+                        iframe: true,
+                        href: this.href,
+                        width: '90%',
+                        height: '90%'
+                    });
+                } else {
+
+                    var sText = this.getAttribute("data-tc-text") ? this.getAttribute("data-tc-text") : 'Are you sure?',
+                        sName = this.getAttribute("data-tc-name") ? this.getAttribute("data-tc-name") : 'Delete this item.';
+
+                    if (!confirm("\n"+ sName + ":\n" + sText + "\n")) {
+                        return false;
+                    }
+                }
+            });
+        },
+        updateVariants: function( oResponse ) {
+
+            var self = this;
+
+            document.getElementById('variants-list').innerHTML = '<p class="ta-c pa-xl"><i class="icon-spin icon-spinner fz-xl"></i></p>'
+
+            $.get(oResponse.data.url, function( sHtml ) {
+
+                document.getElementById('variants-list').innerHTML = sHtml;
+
+                self.bindLinks();
+
+            });
+
+
+            this.modal.close();
+        }
+    };
+});
+
--- src/Elcodi/AdminProductBundle/Form/Type/VariantType.php
+++ src/Elcodi/AdminProductBundle/Form/Type/VariantType.php
@@ -63,7 +63,7 @@ class VariantType extends AbstractPurchasableType
             ))
             ->add('enabled', 'checkbox', array(
                 'required' => false,
-                'label'    => 'Enabled'
+                'label'    => 'Status'
             ));
     }
 
