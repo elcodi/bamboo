0da3de949ea5151cac45e8bae5a1d437e6d3f264
Berny Cantos <xphere@elcodi.com>
2015-02-04 18:48:30 +0100

    fix cs and some minor changes
    - use `elcodi.abstract_` in manager and repository
    - add `use` for global classes like `DateTime`
    - add `AuthorizationFactory` instead of `new Authorization`

---
+++ src/Elcodi/Store/ConnectBundle/Factory/AuthorizationFactory.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Store\ConnectBundle\Factory;
+
+use Elcodi\Component\Core\Factory\Abstracts\AbstractFactory;
+use Elcodi\Store\ConnectBundle\Entity\Interfaces\AuthorizationInterface;
+
+/**
+ * Factory for Authorization entities
+ *
+ * @author Berny Cantos <be@rny.cc>
+ */
+class AuthorizationFactory extends AbstractFactory
+{
+    /**
+     * Creates an Authorization instance
+     *
+     * @return AuthorizationInterface New Authorization entity
+     */
+    public function create()
+    {
+        /**
+         * @var AuthorizationInterface $authorization
+         */
+        $classNamespace = $this->getEntityNamespace();
+        $authorization = new $classNamespace();
+
+        return $authorization;
+    }
+}
