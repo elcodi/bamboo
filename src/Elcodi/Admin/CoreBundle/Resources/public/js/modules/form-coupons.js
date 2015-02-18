7993881114594b5e59713d54b26e67735a88afda
Toni Pinel <tonipinel@gmail.com>
2015-02-18 18:23:54 +0100

    Markup, design and Js for coupons

---
+++ src/Elcodi/Admin/CoreBundle/Resources/public/js/modules/form-coupons.js
@@ -0,0 +1,38 @@
+TinyCore.AMD.define('form-coupons', [], function () {
+	return {
+		onStart: function () {
+
+			var self = this,
+				sTypeValue = document.getElementById('elcodi_admin_coupon_form_type_coupon_type').value;
+
+			oTools.trackEvent('JS_Libraries', 'call', 'form-coupons');
+
+			self.TypeVisibility(sTypeValue);
+
+			$('#elcodi_admin_coupon_form_type_coupon_type').change( function(){
+				self.TypeVisibility( this.value );
+			});
+
+		},
+		TypeVisibility: function (sValue) {
+
+			var ShowElement,
+				hideElement,
+				sNameFixed = 'fixed-amount',
+				sNamePercent = 'percent-amount';
+
+			if (sValue === '1') {
+				ShowElement = sNameFixed;
+				hideElement = sNamePercent;
+			} else {
+				hideElement = sNameFixed;
+				ShowElement = sNamePercent;
+			}
+
+			$('#' + ShowElement).slideDown();
+			$('#' + hideElement).slideUp();
+
+		}
+	};
+});
+
