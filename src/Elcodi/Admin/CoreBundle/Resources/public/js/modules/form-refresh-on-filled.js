061a9de2858d9046b6f9c2d393e5c960009556ba
Toni Pinel <tonipinel@gmail.com>
2015-02-28 01:15:49 +0100

    Refresh payments page on filled

---
+++ src/Elcodi/Admin/CoreBundle/Resources/public/js/modules/form-refresh-on-filled.js
@@ -0,0 +1,33 @@
+TinyCore.AMD.define('form-refresh-on-filled', ['devicePackage'], function () {
+	return {
+		oFields : ['store.paymill_private_key','store.paymill_public_key','store.paypal_web_checkout_recipient'],
+		mediator : TinyCore.Toolbox.request( 'mediator' ),
+		onStart: function () {
+
+			oTools.trackEvent('JS_Libraries', 'call', 'form-refresh-on-filled');
+
+			this.mediator.subscribe( ['save:item'], this.addAndCheck, this );
+		},
+		addAndCheck: function( oResponse ) {
+
+			var bReload = true;
+
+			if (this.oFields.indexOf(oResponse.data.message) === -1) {
+				this.oFields.push(oResponse.data.message);
+			}
+
+			for (var nKey = 0; this.oFields.length > nKey; nKey++) {
+
+				if (document.getElementById(this.oFields[nKey].replace('store_','store.')).value === '') {
+					bReload = false;
+				}
+			}
+
+			if (bReload) {
+				location.reload();
+			}
+
+		}
+	};
+});
+
