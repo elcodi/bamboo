be3ff831c7477ed0e14e6687d719331e174339f4
Javier Eguiluz <javier.eguiluz@gmail.com>
2015-03-12 11:43:52 +0100

    Removed unused use statements

--- src/Elcodi/Admin/PageBundle/Controller/EmailController.php
+++ src/Elcodi/Admin/PageBundle/Controller/EmailController.php
@@ -19,7 +19,6 @@ namespace Elcodi\Admin\PageBundle\Controller;
 
 use Mmoreram\ControllerExtraBundle\Annotation\Entity as EntityAnnotation;
 use Mmoreram\ControllerExtraBundle\Annotation\Form as FormAnnotation;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Form\FormInterface;
