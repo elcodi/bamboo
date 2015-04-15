65ed81f98a401f167cfc0236d6817632f20bb72b
Toni Pinel <tonipinel@gmail.com>
2015-04-15 23:14:41 +0200

    Updates frontendcore to last version

---
+++ src/Elcodi/Admin/CoreBundle/Resources/public/components/frontendcore-js/modules/skin-selector.js
@@ -0,0 +1,30 @@
+
+FrontendCore.define('skin-selector', [], function () {
+	return {
+		onStart: function () {
+			var oSelect = document.getElementById('skin-selector'),
+				oCSS = document.getElementById('skin-css'),
+				sCSSPath = oCSS.href,
+				sHash = window.location.hash.replace('#','');
+
+			$(oSelect).change( function(){
+
+				if (this.value.indexOf('...') === -1 ) {
+					oCSS.href = sCSSPath.replace('main','skins/' + this.value);
+					window.location.hash = '#' + this.value;
+				} else {
+					oCSS.href = sCSSPath;
+					window.location.hash = '';
+				}
+
+
+			});
+
+			if ( sHash !== '') {
+				oSelect.value = sHash;
+				$(oSelect).change();
+			};
+
+		}
+	};
+});
