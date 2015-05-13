074fff043f4ba4b4bd3e46f8521e62747ff5cf70
Toni Pinel <tonipinel@gmail.com>
2015-05-13 11:07:39 +0200

    Updated Frontendcore and improved colors of standard buttons

--- src/Elcodi/Admin/CoreBundle/Resources/public/components/plupload/examples/upload.php
+++ src/Elcodi/Admin/CoreBundle/Resources/public/components/plupload/examples/upload.php
@@ -1,32 +1,33 @@
 <?php
-
-/*
- * This file is part of the Elcodi package.
+/**
+ * upload.php
  *
- * Copyright (c) 2014-2015 Elcodi.com
+ * Copyright 2013, Moxiecode Systems AB
+ * Released under GPL License.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * Feel free to edit as you please, and have fun.
- *
- * @author Marc Morera <yuhu@mmoreram.com>
- * @author Aldo Chiecchia <zimage@tiscali.it>
- * @author Elcodi Team <tech@elcodi.com>
+ * License: http://www.plupload.com/license
+ * Contributing: http://www.plupload.com/contributing
  */
 
+#!! IMPORTANT: 
+#!! this file is just an example, it doesn't incorporate any security checks and 
+#!! is not recommended to be used in production environment as it is. Be sure to 
+#!! revise it and customize to your needs.
+
+
+// Make sure file is not cached (as it happens for example on iOS devices)
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-/*
+/* 
 // Support CORS
 header("Access-Control-Allow-Origin: *");
 // other CORS headers if any...
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    exit; // finish preflight CORS requests here
+	exit; // finish preflight CORS requests here
 }
 */
 
@@ -45,16 +46,16 @@ $maxFileAge = 5 * 3600; // Temp file age in seconds
 
 // Create target dir
 if (!file_exists($targetDir)) {
-    @mkdir($targetDir);
+	@mkdir($targetDir);
 }
 
 // Get a file name
 if (isset($_REQUEST["name"])) {
-    $fileName = $_REQUEST["name"];
+	$fileName = $_REQUEST["name"];
 } elseif (!empty($_FILES)) {
-    $fileName = $_FILES["file"]["name"];
+	$fileName = $_FILES["file"]["name"];
 } else {
-    $fileName = uniqid("file_");
+	$fileName = uniqid("file_");
 }
 
 $filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
@@ -63,50 +64,52 @@ $filePath = $targetDir . DIRECTORY_SEPARATOR . $fileName;
 $chunk = isset($_REQUEST["chunk"]) ? intval($_REQUEST["chunk"]) : 0;
 $chunks = isset($_REQUEST["chunks"]) ? intval($_REQUEST["chunks"]) : 0;
 
-// Remove old temp files
+
+// Remove old temp files	
 if ($cleanupTargetDir) {
-    if (!is_dir($targetDir) || !$dir = opendir($targetDir)) {
-        die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
-    }
+	if (!is_dir($targetDir) || !$dir = opendir($targetDir)) {
+		die('{"jsonrpc" : "2.0", "error" : {"code": 100, "message": "Failed to open temp directory."}, "id" : "id"}');
+	}
 
-    while (($file = readdir($dir)) !== false) {
-        $tmpfilePath = $targetDir . DIRECTORY_SEPARATOR . $file;
+	while (($file = readdir($dir)) !== false) {
+		$tmpfilePath = $targetDir . DIRECTORY_SEPARATOR . $file;
 
-        // If temp file is current file proceed to the next
-        if ($tmpfilePath == "{$filePath}.part") {
-            continue;
-        }
+		// If temp file is current file proceed to the next
+		if ($tmpfilePath == "{$filePath}.part") {
+			continue;
+		}
 
-        // Remove temp file if it is older than the max age and is not the current file
-        if (preg_match('/\.part$/', $file) && (filemtime($tmpfilePath) < time() - $maxFileAge)) {
-            @unlink($tmpfilePath);
-        }
-    }
-    closedir($dir);
-}
+		// Remove temp file if it is older than the max age and is not the current file
+		if (preg_match('/\.part$/', $file) && (filemtime($tmpfilePath) < time() - $maxFileAge)) {
+			@unlink($tmpfilePath);
+		}
+	}
+	closedir($dir);
+}	
+
 
 // Open temp file
 if (!$out = @fopen("{$filePath}.part", $chunks ? "ab" : "wb")) {
-    die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
+	die('{"jsonrpc" : "2.0", "error" : {"code": 102, "message": "Failed to open output stream."}, "id" : "id"}');
 }
 
 if (!empty($_FILES)) {
-    if ($_FILES["file"]["error"] || !is_uploaded_file($_FILES["file"]["tmp_name"])) {
-        die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
-    }
+	if ($_FILES["file"]["error"] || !is_uploaded_file($_FILES["file"]["tmp_name"])) {
+		die('{"jsonrpc" : "2.0", "error" : {"code": 103, "message": "Failed to move uploaded file."}, "id" : "id"}');
+	}
 
-    // Read binary input stream and append it to temp file
-    if (!$in = @fopen($_FILES["file"]["tmp_name"], "rb")) {
-        die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
-    }
-} else {
-    if (!$in = @fopen("php://input", "rb")) {
-        die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
-    }
+	// Read binary input stream and append it to temp file
+	if (!$in = @fopen($_FILES["file"]["tmp_name"], "rb")) {
+		die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+	}
+} else {	
+	if (!$in = @fopen("php://input", "rb")) {
+		die('{"jsonrpc" : "2.0", "error" : {"code": 101, "message": "Failed to open input stream."}, "id" : "id"}');
+	}
 }
 
 while ($buff = fread($in, 4096)) {
-    fwrite($out, $buff);
+	fwrite($out, $buff);
 }
 
 @fclose($out);
@@ -114,8 +117,8 @@ while ($buff = fread($in, 4096)) {
 
 // Check if file has been uploaded
 if (!$chunks || $chunk == $chunks - 1) {
-    // Strip the temp .part suffix off
-    rename("{$filePath}.part", $filePath);
+	// Strip the temp .part suffix off 
+	rename("{$filePath}.part", $filePath);
 }
 
 // Return Success JSON-RPC response
