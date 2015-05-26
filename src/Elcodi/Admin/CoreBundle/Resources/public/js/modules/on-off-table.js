4bdfe78c24d4374fae338d3e0c8088ed89f12644
Toni Pinel <tonipinel@gmail.com>
2015-05-26 11:51:18 +0200

    Reactivate master language if error

--- src/Elcodi/Admin/CoreBundle/Resources/public/js/modules/on-off-table.js
+++ src/Elcodi/Admin/CoreBundle/Resources/public/js/modules/on-off-table.js
@@ -1,8 +1,16 @@
 FrontendCore.define('on-off-table', ['devicePackage' ], function () {
     return {
+	    oMasterLanguage : '',
+	    setMasterLanguage: function() {
+		    this.oMasterLanguage = $('[name=language-master]:checked')[0];
+	    },
         onStart: function () {
 
             FrontendCore.requireAndStart('notification');
+
+	        var self = this;
+
+	        self.setMasterLanguage();
 
             $('.switch input').each( function(){
 
@@ -15,14 +23,20 @@ FrontendCore.define('on-off-table', ['devicePackage' ], function () {
 	                    sUrl = this.checked === true ? document.getElementById('enable-' + this.id).value : document.getElementById('disable-' + this.id).value;
 
                     $.ajax({
-                        url: sUrl,
-                        type:  'post'
+	                    url: sUrl,
+	                    type: 'post'
+                    }).done( function() {
+	                    self.setMasterLanguage();
                     }).fail( function( response ) {
 
 	                    if ( sValue === true ) {
 		                    oInput.checked = false;
 	                    } else {
 		                    oInput.checked = true;
+	                    }
+
+	                    if ( oInput.name === 'language-master' ) {
+		                    self.oMasterLanguage.checked = true;
 	                    }
 
                         var sMessage = response.responseJSON.message !== undefined ? response.responseJSON.message : 'Sorry, something was wrong.';
