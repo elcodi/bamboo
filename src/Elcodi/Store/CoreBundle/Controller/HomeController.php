4524d7edf3638656f27dcefae5d67323aaac6d67
Fabio Lorenzini <fabio.lorenzini@packetloss.it>
2016-05-30 10:31:10 +0200

    using getUseStock() value to load home products

--- src/Elcodi/Store/CoreBundle/Controller/HomeController.php
+++ src/Elcodi/Store/CoreBundle/Controller/HomeController.php
@@ -17,43 +17,42 @@
 
 namespace Elcodi\Store\CoreBundle\Controller;
 
+use Elcodi\Store\CoreBundle\Controller\Traits\TemplateRenderTrait;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-
-use Elcodi\Store\CoreBundle\Controller\Traits\TemplateRenderTrait;
 
 /**
  * Home controller
  *
  * This class should only contain home actions
  */
-class HomeController extends Controller
-{
-    use TemplateRenderTrait;
+class HomeController extends Controller {
+	use TemplateRenderTrait;
 
-    /**
-     * Home page.
-     *
-     * @return Response Response
-     *
-     * @Route(
-     *      path = "/",
-     *      name = "store_homepage",
-     *      methods = {"GET"}
-     * )
-     */
-    public function homeAction()
-    {
-        $purchasables = $this
-            ->get('elcodi.repository.purchasable')
-            ->getHomePurchasables(9, true);
+	/**
+	 * Home page.
+	 *
+	 * @return Response Response
+	 *
+	 * @Route(
+	 *      path = "/",
+	 *      name = "store_homepage",
+	 *      methods = {"GET"}
+	 * )
+	 */
+	public function homeAction() {
+		$useStock = $this->get('elcodi.store')->getUseStock();
 
-        return $this->renderTemplate(
-            'Pages:home-view.html.twig',
-            [
-                'purchasables' => $purchasables,
-            ]
-        );
-    }
+		$purchasables = $this
+			->get('elcodi.repository.purchasable')
+			->getHomePurchasables(9, $useStock);
+
+		return $this->renderTemplate(
+			'Pages:home-view.html.twig',
+			[
+				'purchasables' => $purchasables,
+			]
+		);
+	}
 }
